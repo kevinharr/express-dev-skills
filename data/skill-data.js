b8817248a7e6c16f5a9448348bc45a9e472a9a2a
kevinharr <kevinharr@verizon.net>
2023-01-04 22:52:03 -0500

    setup skill find function

--- data/skill-data.js
+++
@@ -1,9 +0,0 @@
-const skills = [
-    {type: 'Java', proficient: true, _id: 125223},
-    {type: 'Python', proficient: false, _id: 127904},
-    {type: 'C#/.Net', proficient: false, _id: 139608},
-  ]
-  
-  export { 
-      skills
-  }
