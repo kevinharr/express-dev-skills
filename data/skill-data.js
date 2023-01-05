f58bf736b254071dc95dfbc6b4b912dc413c73c0
kevinharr <kevinharr@verizon.net>
2023-01-04 21:38:06 -0500

    refactor router

--- data/skill-data.js
+++ data/skill-data.js
@@ -1,9 +1,9 @@
-skills todos = [
-    {text: 'Feed llama', done: true, _id: 125223},
-    {text: 'Sleep under the stars', done: false, _id: 127904},
-    {text: 'Buy milk', done: false, _id: 139608},
+const skills = [
+    {type: 'Java', proficient: true, _id: 125223},
+    {type: 'Python', proficient: false, _id: 127904},
+    {type: 'C#/.Net', proficient: false, _id: 139608},
   ]
   
   export { 
-      todos
+      skills
   }
